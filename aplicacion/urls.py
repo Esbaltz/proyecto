350662370428b1605afe50a2449871b4eb059655
Elías <el.antican@duocuc.cl>
2024-05-28 18:21:25 -0400

    primeras-vistas

--- aplicacion/urls.py
+++ aplicacion/urls.py
@@ -1,10 +1,13 @@
 #URLS APLICACION
 from django.urls import path, include
-from .views import index, tienda, gral_libro
+from .views import index, tienda, gral_libro, inf_pago, carrito, domicilio
 
 #URLS DE SITIO
 urlpatterns = [
     path('', index, name='index'),
     path('tienda/', tienda, name='tienda'),
     path('pagina_general_libro/', gral_libro, name='libro'),
+    path('inf_pago/', inf_pago, name='inf_pago'),
+    path('pagina_carrito_general/', carrito, name='carrito'),
+    path('pagina_domicilio_y_pagar/', domicilio, name='domicilio'),
 ]
--- aplicacion/views.py
+++ aplicacion/views.py
@@ -12,3 +12,13 @@ def tienda(request):
 def gral_libro(request):
     return render(request, 'aplicacion/pagina_general_libro.html')
 
+def inf_pago(request):
+    return render(request, 'aplicacion/inf_pago.html')
+
+def carrito(request):
+    return render(request, 'aplicacion/pagina_carrito_general.html')
+
+def domicilio(request):
+    return render(request, 'aplicacion/pagina_domicilio_y_pagar.html')
+
+
