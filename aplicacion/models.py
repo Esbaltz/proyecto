6ab536dd7f93a7505a626487c0abfc6694573463
CETECOM <cetecom_conce@duoc.cl>
2024-05-31 10:13:52 -0400

    pruebas-modelos

--- aplicacion/models.py
+++ aplicacion/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Usuario:
+    id=models.CharField(max_length=12, primary_key=True, null=False)
+    nombre=models.CharField(max_length=30, null=False)
+    correo=models.CharField(max_length=80, null=False)
+    direccion=models.CharField(max_length=30, null=False)
+    contraseña=models.CharField(max_length=30, null=False)
--- sitio/settings.py
+++ sitio/settings.py
@@ -112,6 +112,11 @@ USE_I18N = True
 
 USE_TZ = True
 
+# media files
+MEDIA_URL='\media'
+
+import os 
+MEDIA_ROOT=os.path.join(BASE_DIR, 'proyecto\media')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
