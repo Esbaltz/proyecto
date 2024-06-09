2eae7ab90d35663af4ae47bc217050d02f266060
bENJA <be.cortesp@duocuc.cl>
2024-06-08 18:28:49 -0600

    actualizacaicon_ramav3

--- aplicacion/listas.py
+++ aplicacion/listas.py
@@ -18,3 +18,28 @@ SUBTIPO_LIBRO = {
     },
     "Sin subtipo": "Sin Subtipo",
 }
+
+TIPO_REGION = {
+    ("Arica y Parinacota", "Arica y Parinacota"),
+    ("Tarapacá", "Tarapacá"),
+    ("Antofagasta", "Antofagasta"),
+    ("Atacama", "Atacama"),
+    ("Coquimbo", "Coquimbo"),
+    ("Valparaíso", "Valparaíso"),
+    ("Metropolitana de Santiago", "Metropolitana de Santiago"),
+    ("Libertador General Bernardo O'Higgins", "Libertador General Bernardo O'Higgins"),
+    ("Maule", "Maule"),
+    ("Ñuble", "Ñuble"),
+    ("Biobío", "Biobío"),
+    ("La Araucanía", "La Araucanía"),
+    ("Los Ríos", "Los Ríos"),
+    ("Los Lagos", "Los Lagos"),
+    ("Aysén del General Carlos Ibáñez del Campo", "Aysén del General Carlos Ibáñez del Campo"),
+}
+
+TIPO_TARGETA={
+        ('VISA', 'Visa'),
+        ('MC', 'MasterCard'),
+        ('AMEX', 'American Express'),
+        ('DISC', 'Discover'),
+}
--- aplicacion/models.py
+++ aplicacion/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
-from .listas import TIPO_GENERO, SUBTIPO_LIBRO
+from django.core.validators import MinLengthValidator
+from .listas import TIPO_GENERO, SUBTIPO_LIBRO, TIPO_REGION, TIPO_TARGETA
 # Create your models here.
 class Libro(models.Model):
     #no se si dejar el isbn como clave primaria o ponerle un id para diferenciarlos
@@ -11,6 +12,10 @@ class Libro(models.Model):
     tipo=models.CharField(max_length=30, choices=TIPO_GENERO)
     subtipo=models.CharField(max_length=20, choices=SUBTIPO_LIBRO, default='Sin subtipo')
     es_best_seler = models.BooleanField(default=True)
+    #ladireccion va con un varchar
+    #la region va con el tema de DE LOS TIPOS
+    nombre=models.CharField(max_length=50, null=False)
+    tipo_region=models.CharField(max_length=40, validators=[MinLengthValidator(7)], choices=TIPO_REGION)
 
     def __str__(self):
         return f"nombre {self.nombre} precio: {self.precio} "
@@ -19,7 +24,7 @@ class Usuario(models.Model):
     id=models.CharField(max_length=10, primary_key=True, null=False)
     nombre=models.CharField(max_length=50, null=False)
     correo=models.CharField(max_length=100, null=False)
-    contraseña=models.CharField(max_length=100, null=False)
+    contraseña=models.CharField(max_length=100, null=False) 
 
 class Carrito(models.Model):
 
@@ -47,5 +52,19 @@ class libro_oferta(models.Model):
     
 
 
-#no se si agregar la fecha en el detalle del pedido y reutilizarlo como historial de compra
+#se reutiliza el detalle del pedido para TENER LA INFORMACION DEL HISTORICO DE LAS COMPRAS QUE VE EL ADMIN Y EL MISMO USUARIO
 #o ocupar lo que me ayudo chat gpt y hacerlo de la forma de abajo :v
+#falta el como validar bien la FECHA DE LA TARGETA:
+
+class targeta(models.Model):
+        precio=models.IntegerField(validators=[MinValueValidator(13),MaxValueValidator(18)])
+        #dudds de como validar la fecha de la targeta
+        f_vencimiento=models.DateField("Fecha vencimiento de la targeta")
+        #AGREGAR QUE EL USUARIO ELIJA EL TIPO DE TARGETA QUE ESTA INGRESANDO EN EL HTML Y VERIFICAR CON EL JS
+        tipo_targeta= models.CharField(max_length=30, choices=TIPO_TARGETA)
+
+#Hacer el boton de tipo targeta reciclando el boton del domicilio para los 4 tipos de targeta de forma manual tipo boton deslizable
+
+#tambien falta hacer los def para que las clases devuelvan la infrormacion, pero al final XD
+
+#Debemos realizar el logo de nuevo que contenga: .¿(despues XD)
