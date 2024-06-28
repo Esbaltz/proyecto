b9dad97d87b412e2c578fe7b32e41517f79db04f
Elías <el.antican@duocuc.cl>
2024-06-27 22:31:31 -0400

    cerrar_sesion

--- aplicacion/static/aplicacion/js/messages.js
+++ aplicacion/static/aplicacion/js/messages.js
@@ -30,3 +30,36 @@ const eliminarFila = (url, nombreTabla) => {
         }
     });
 }
+
+const cerrarSesion = (url) => {
+    iziToast.question({
+        timeout: 20000,
+        close: false,
+        overlay: true,
+        displayMode: 'once',
+        id: 'question',
+        zindex: 999,
+        title: 'Calmao',
+        message: '¿Seguro que quieres cerrar sesión?',
+        position: 'center',
+        buttons: [
+            ['<button><b>Si, si quiero</b></button>', function (instance, toast) {
+     
+                instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+                window.location.href = url;
+     
+            }, true],
+            ['<button>Era broma</button>', function (instance, toast) {
+     
+                instance.hide({ transitionOut: 'fadeOut' }, toast, 'button');
+     
+            }],
+        ],
+        onClosing: function(instance, toast, closedBy){
+            console.info('Closing | closedBy: ' + closedBy);
+        },
+        onClosed: function(instance, toast, closedBy){
+            console.info('Closed | closedBy: ' + closedBy);
+        }
+    });
+}
